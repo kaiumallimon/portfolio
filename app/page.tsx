825645d5ed10ce3b1ff4e97ad61d20526cf1f9de
Kaium Al Limon <limon.3cs@gmail.com>
2025-11-04 16:30:31 +0600

    Fix layout issue by changing width from 'w-screen' to 'w-full' in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen w-screen bg-white">
+    <div className="min-h-screen w-full bg-white">
       <Hero/>
       <ScrollDemo/>
     </div>
