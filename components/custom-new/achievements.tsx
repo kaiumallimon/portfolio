ac8b82e530a271f2ce56bf2f1e659d407b7b0502
Kaium Al Limon <limon.3cs@gmail.com>
2026-02-12 04:40:31 +0600

    feat: add AchievementsSection component to display project achievements

---
+++ components/custom-new/achievements.tsx
@@ -0,0 +1,42 @@
+
+
+export default function AchievementsSection() {
+  const achievements = [
+    {
+      title: "UIU CSE Project Show | Fall '24",
+      award: "1st Runner-Up – Software Engineering Lab",
+      date: "27 January 2025",
+      project: "MediTouch",
+      team: "Team Bcrypt",
+      image: ["/software-lab.jpg", "/software-lab-2.jpg"],
+    },
+    {
+      title: "UIU CSE Project Show | Summer '24",
+      award: "Champion – System Analysis and Design ",
+      date: "8 September 2024",
+      project: "MediTouch",
+      team: "Team Bcrypt",
+      image: ["/sad-lab.png", "/sad-lab-2.jpg"],
+    },
+    {
+      title: "UIU CSE Project Show | Spring '24",
+      award: "1st Runner-Up – Database Management System (DBMS)",
+      date: "3 June 2024",
+      project: "Pharmabrew",
+      team: "Team Bcrypt",
+      image: ["/dbms-lab.png", "/dbms-lab-2.png"],
+    },
+    {
+      title: "UIU CSE Project Show | Spring '23",
+      award: "2nd Runner-Up – Advanced Object-Oriented Programming (AOOP) ",
+      date: "3 May 2023",
+      project: "Wayout",
+      team: "Team Tripod",
+      image: ["/aoop-lab.jpg", "/aoop-lab-2.jpg"],
+    },
+  ];
+
+  return (
+    <div></div>
+  );
+}
