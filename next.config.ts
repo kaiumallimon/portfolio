ab582667e9143c94f2a6c37f93a364242dc5e4fa
Kaium Al Limon <limon.3cs@gmail.com>
2026-07-16 03:26:24 +0600

    feat: move serverActions configuration under experimental in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -17,8 +17,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  serverActions: {
-    bodySizeLimit: "5mb",
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
+    },
   },
 };
 
